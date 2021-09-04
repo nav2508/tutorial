55c5222651c74745fe3e698f84fcddc4021e6932
Navya Manjari <navyamanjari.hyd@gmail.com>
2021-09-04 11:05:49 +0530

    first commit

---
+++ srms-version-1.0/header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>STUDENT RESULT PANEL</title>
+
+	    <!-- Custom styles for this page -->
+	    <link href="vendor/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+	    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+	    <link rel="stylesheet" type="text/css" href="vendor/parsley/parsley.css"/>
+	    <style>
+	    	.border-top { border-top: 1px solid #e5e5e5; }
+			.border-bottom { border-bottom: 1px solid #e5e5e5; }
+
+			.box-shadow { box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); }
+	    </style>
+	</head>
+	<body>
+		
+
+	    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+	      	<h1 class="display-4">Student Results</h1>
+	    </div>
+	    <br />
+	    <br />
+	    <div class="container">
